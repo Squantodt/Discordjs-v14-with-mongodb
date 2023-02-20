6f6694de854020d79c59e8a8c7b2f9b7327c0de2
Squantodt <squanto.de.temmerman@student.howest.be>
2023-02-20 21:13:04 +0100

    create database connection on startup

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,20 @@
 const { Events } = require('discord.js');
+const mongoose = require('mongoose');
+const mongodbURL = process.env.mongodbURL;
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
 		console.log(`Ready! Logged in as ${client.user.tag}`);
+		await mongoose.connect(mongodbURL || '', {
+			keepAlive: true,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		});
+
+		if (mongoose.connect) {
+			console.log('The database is up and running');
+		}
 	},
 };
